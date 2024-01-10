f6f3da7970e64d524bc6df94e1ef6909292f7c83
ThaSMorato <tha.s.morato@gmail.com>
2024-01-10 12:02:17 -0300

    refact: env service and env module

---
+++ src/infra/env/env.service.ts
@@ -0,0 +1,12 @@
+import { Env } from './env'
+import { ConfigService } from '@nestjs/config'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class EnvService {
+  constructor(private configService: ConfigService<Env, true>) {}
+
+  get<Key extends keyof Env>(key: Key) {
+    return this.configService.get<Key>(key, { infer: true })
+  }
+}
