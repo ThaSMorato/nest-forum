738d79f0c236f6d1f26767b44e553f3f9c0e58e9
ThaSMorato <tha.s.morato@gmail.com>
2024-01-11 10:48:21 -0300

    feat: answer question controller

---
+++ src/infra/http/controllers/answer-question.controller.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { QuestionFactory } from '$/factories/make-question'
+import { StudentFactory } from '$/factories/make-student'
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Answer question (E2E)', () => {
+  let app: INestApplication
+  let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /questions/:questionId/answers', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: String(user.id) })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const response = await request(app.getHttpServer())
+      .post(`/questions/${question.id}/answers`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New Answer Content',
+      })
+
+    const answerOnDatabase = await prisma.answer.findFirst({
+      where: {
+        questionId: String(question.id),
+      },
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(answerOnDatabase).toBeTruthy()
+    expect(answerOnDatabase).toEqual(
+      expect.objectContaining({
+        content: 'New Answer Content',
+      }),
+    )
+  })
+})
